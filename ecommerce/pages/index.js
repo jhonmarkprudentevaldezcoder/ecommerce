2ff0c1f6b5322c9d6de19d765be145401ac837a0
jhon mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-03-17 02:18:17 +0800

    display products

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,9 +1,17 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
 import Layout from '@/components/Layout';
-
-const inter = Inter({ subsets: ['latin'] });
+import data from '@/utils/data';
+import Productitem from '@/components/Productitem';
 
 export default function Home() {
-  return <Layout title="home page">HOME PAGE</Layout>;
+  return (
+    <Layout title="home page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => {
+          <Productitem product={product} key={product.slug} />;
+        })}
+      </div>
+    </Layout>
+  );
 }
---
+++ ecommerce/utils/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'FREE SHIRT',
+      slug: 'FREE-SHIRT',
+      category: 'SHIRT',
+      image: '/images/tshirtC.png',
+      price: 70,
+      brand: 'NIKE',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'popular shirt',
+    },
+    {
+      name: 'FREE SHIRT',
+      slug: 'FREE-SHIRT',
+      category: 'SHIRT',
+      image: '/images/tshirtA.png',
+      price: 70,
+      brand: 'NIKE',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'popular shirt',
+    },
+    {
+      name: 'FREE SHIRT',
+      slug: 'FREE-SHIRT',
+      category: 'SHIRT',
+      image: '/images/tshirtB.png',
+      price: 70,
+      brand: 'NIKE',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'popular shirt',
+    },
+  ],
+};
+
+export default data;
