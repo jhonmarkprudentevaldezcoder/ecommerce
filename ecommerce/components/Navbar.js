25bf24a1b58db64c5ab2daa2fb58ca207b143da9
jhon mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-03-18 13:27:53 +0800

    update login

--- ecommerce/components/Navbar.js
+++ ecommerce/components/Navbar.js
@@ -1,17 +1,28 @@
 import Link from 'next/link';
 import React, { useContext, useState, useEffect } from 'react';
 import { Store } from '@/utils/Store';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import 'react-toastify/dist/ReactToastify.css';
+import { Menu } from '@headlessui/react';
+import DropdownLink from './DropdownLink';
+import Cookies from 'js-cookie';
 
 export default function Navbar() {
   const { status, data: session } = useSession();
-  const { state } = useContext(Store);
+  const { state, dispatch } = useContext(Store);
   const { cart } = state;
   const [cartItemsCount, setCartItemsCount] = useState(0);
+
   useEffect(() => {
     setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
   }, [cart.cartItems]);
+
+  const LogoutClickHandler = () => {
+    Cookies.remove('Cart');
+    dispatch({ type: 'CART_RESET' });
+    signOut({ callbackUrl: '/login' });
+  };
+
   return (
     <nav className="flex h-12 justify-between shadow-md items-center px-4">
       <Link href="/" className="text-lg font-bold">
@@ -30,7 +41,33 @@ export default function Navbar() {
         {status === 'loading' ? (
           'Loading'
         ) : session?.user ? (
-          session.user.name
+          /*    */
+          <Menu as="div" className="relative inline-block">
+            <Menu.Button className="text-teal-800 font-bold">
+              {session.user.name}
+            </Menu.Button>
+            <Menu.Items className="absolute right-0 w-56 origin-top-right shadow-lg bg-white">
+              <Menu.Item>
+                <DropdownLink className="dropdown-link" href="/Profile">
+                  Profile
+                </DropdownLink>
+              </Menu.Item>
+              <Menu.Item>
+                <DropdownLink
+                  className="dropdown-link"
+                  onClick={LogoutClickHandler}
+                  href="#"
+                >
+                  Logout
+                </DropdownLink>
+              </Menu.Item>
+              <Menu.Item>
+                <DropdownLink className="dropdown-link" href="/Profile">
+                  History
+                </DropdownLink>
+              </Menu.Item>
+            </Menu.Items>
+          </Menu>
         ) : (
           <Link href="/login" className="p-2">
             LOGIN
--- ecommerce/utils/Store.js
+++ ecommerce/utils/Store.js
@@ -31,6 +31,14 @@ function reducer(state, action) {
       Cookies.set('Cart', JSON.stringify({ ...state.cart, cartItems }));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
+    case 'CART_RESET': {
+      return {
+        ...state,
+        cart: {
+          cartItems: [],
+        },
+      };
+    }
     default:
       return state;
   }
